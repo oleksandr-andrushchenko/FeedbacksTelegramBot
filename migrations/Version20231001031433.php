04bca5366346f89ae2ab15ea2a6f71de1fb5345b
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-09-30 21:54:25 -0600

    missed files

---
+++ migrations/Version20231001031433.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231001031433 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users CHANGE location_latitude location_latitude VARCHAR(20) DEFAULT NULL, CHANGE location_longitude location_longitude VARCHAR(20) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users CHANGE location_latitude location_latitude VARCHAR(17) DEFAULT NULL, CHANGE location_longitude location_longitude VARCHAR(17) DEFAULT NULL');
+    }
+}
