8d5361c930e4baa2d17da636c9cb07fa33114f24
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-10-10 01:20:25 -0600

    telegram bots & channels import: csv file walker implemented, deletetions improved

--- src/Service/Telegram/Bot/TelegramBotImporter.php
+++ src/Service/Telegram/Bot/TelegramBotImporter.php
@@ -7,14 +7,12 @@ namespace App\Service\Telegram\Bot;
 use App\Entity\ImportResult;
 use App\Enum\Telegram\TelegramBotGroupName;
 use App\Repository\Telegram\Bot\TelegramBotRepository;
+use App\Service\CsvFileWalker;
 use App\Service\Intl\CountryProvider;
 use App\Service\Intl\LocaleProvider;
 use App\Service\Telegram\Bot\Api\TelegramBotDescriptionsSyncer;
 use App\Service\Telegram\Bot\Api\TelegramBotWebhookSyncer;
 use App\Transfer\Telegram\TelegramBotTransfer;
-use InvalidArgumentException;
-use LogicException;
-use RuntimeException;
 use Throwable;
 
 class TelegramBotImporter
@@ -28,6 +26,7 @@ class TelegramBotImporter
         private readonly TelegramBotWebhookSyncer $webhookUpdater,
         private readonly CountryProvider $countryProvider,
         private readonly LocaleProvider $localeProvider,
+        private readonly CsvFileWalker $walker,
         private readonly string $stage,
     )
     {
@@ -35,139 +34,116 @@ class TelegramBotImporter
 
     public function importTelegramBots(string $filename, callable $logger = null): ImportResult
     {
-        if (!file_exists($filename)) {
-            throw new InvalidArgumentException(sprintf('"%s" file is not exists', $filename));
-        }
-
-        $handle = fopen($filename, 'r');
-
-        if ($handle === false) {
-            throw new RuntimeException(sprintf('Unable to open "%s" file', $filename));
-        }
-
         $result = new ImportResult();
 
-        try {
-            $usernames = [];
-            $mandatoryColumns = [
-                'skip',
-                'group',
-                'username',
-                'name',
-                'token',
-                'stage',
-                'country',
-                'locale',
-                'primary',
-                'admin_id',
-                'admin_only',
-            ];
-
-            $logger = $logger ?? fn (string $message) => null;
-
-            $columns = fgetcsv($handle);
-            $count = count($columns);
-
-            $index = 1;
-
-            while (($row = fgetcsv($handle)) !== false) {
-                $index++;
-
-                if (!isset($row[0]) || [null] === $row) {
-                    continue;
-                }
-
-                $rowCount = count($row);
-
-                if ($count !== $rowCount) {
-                    throw new LogicException(sprintf('Row #%d has wrong number of columns. Should have %d columns, got %d', $index, $count, $rowCount));
-                }
-
-                $data = array_combine($columns, $row);
-
-                foreach ($mandatoryColumns as $mandatoryColumn) {
-                    if (!array_key_exists($mandatoryColumn, $data)) {
-                        throw new LogicException(sprintf('Row #%d has not "%s" column', $index, $mandatoryColumn));
-                    }
-                }
-
-                if ($data['stage'] !== $this->stage) {
-                    continue;
-                }
-
-                if ($data['skip'] === '1') {
-                    continue;
-                }
-
-                $transfer = (new TelegramBotTransfer($data['username']))
-                    ->setGroup(TelegramBotGroupName::fromName($data['group']))
-                    ->setName($data['name'])
-                    ->setToken($data['token'])
-                    ->setCountry($this->countryProvider->getCountry($data['country']))
-                    ->setLocale($this->localeProvider->getLocale($data['locale']))
-                    ->setPrimary($data['primary'] === '1')
-                    ->setAdminIds(empty($data['admin_id']) ? null : [$data['admin_id']])
-                    ->setAdminOnly($data['admin_only'] === '1')
-                ;
-
-                $bot = $this->repository->findAnyOneByUsername($transfer->getUsername());
-
-                $message = $transfer->getUsername();
+        $bots = $this->repository->findAll();
+        $usernames = $this->getUsernames($filename);
+        foreach ($bots as $bot) {
+            if (!in_array($bot->getUsername(), $usernames, true) && !$this->remover->telegramBotRemoved($bot)) {
+                $this->remover->removeTelegramBot($bot);
+                $message = $bot->getUsername();
                 $message .= ': [OK] ';
-
-                if ($bot === null) {
-                    $bot = $this->creator->createTelegramBot($transfer);
-                    $message .= 'created';
-                    $result->incCreatedCount();
-                } else {
-                    $this->updater->updateTelegramBot($bot, $transfer);
-                    $message .= 'updated';
-                    $result->incUpdatedCount();
-
-                    if ($this->remover->telegramBotRemoved($bot)) {
-                        $this->remover->undoTelegramBotRemove($bot);
-                        $message .= '; [OK] restored';
-                        $result->incRestoredCount();
-                    }
-                }
-
-                $usernames[] = $bot->getUsername();
-
-                if ($bot !== null && !$bot->descriptionsSynced() && !$this->remover->telegramBotRemoved($bot)) {
-                    try {
-                        $this->textsUpdater->syncTelegramDescriptions($bot);
-                        $message .= '; [OK] descriptions';
-                    } catch (Throwable $exception) {
-                        $message .= '; [FAIL] descriptions - ' . $exception->getMessage();
-                    }
-                }
-                if ($bot !== null && !$bot->webhookSynced() && !$this->remover->telegramBotRemoved($bot)) {
-                    try {
-                        $this->webhookUpdater->syncTelegramWebhook($bot);
-                        $message .= '; [OK] webhook';
-                    } catch (Throwable $exception) {
-                        $message .= '; [FAIL] webhook - ' . $exception->getMessage();
-                    }
-                }
-
+                $message .= 'deleted';
+                $result->incDeletedCount();
                 $logger($message);
             }
-
-            $bots = $this->repository->findAll();
-            foreach ($bots as $bot) {
-                if (!in_array($bot->getUsername(), $usernames, true) && !$this->remover->telegramBotRemoved($bot)) {
-                    $this->remover->removeTelegramBot($bot);
-                    $message = $bot->getUsername();
-                    $message .= ': [OK] ';
-                    $message .= 'deleted';
-                    $result->incDeletedCount();
-                    $logger($message);
-                }
-            }
-        } finally {
-            fclose($handle);
         }
 
+        $logger = $logger ?? fn (string $message) => null;
+
+        $this->walk($filename, function ($data) use ($result, $logger): void {
+            $transfer = (new TelegramBotTransfer($data['username']))
+                ->setGroup(TelegramBotGroupName::fromName($data['group']))
+                ->setName($data['name'])
+                ->setToken($data['token'])
+                ->setCountry($this->countryProvider->getCountry($data['country']))
+                ->setLocale($this->localeProvider->getLocale($data['locale']))
+                ->setPrimary($data['primary'] === '1')
+                ->setAdminIds(empty($data['admin_id']) ? null : [$data['admin_id']])
+                ->setAdminOnly($data['admin_only'] === '1')
+            ;
+
+            $bot = $this->repository->findAnyOneByUsername($transfer->getUsername());
+
+            $message = $transfer->getUsername();
+            $message .= ': [OK] ';
+
+            if ($bot === null) {
+                $bot = $this->creator->createTelegramBot($transfer);
+                $message .= 'created';
+                $result->incCreatedCount();
+            } else {
+                $this->updater->updateTelegramBot($bot, $transfer);
+                $message .= 'updated';
+                $result->incUpdatedCount();
+
+                if ($this->remover->telegramBotRemoved($bot)) {
+                    $this->remover->undoTelegramBotRemove($bot);
+                    $message .= '; [OK] restored';
+                    $result->incRestoredCount();
+                }
+            }
+
+            if ($bot !== null && !$bot->descriptionsSynced() && !$this->remover->telegramBotRemoved($bot)) {
+                try {
+                    $this->textsUpdater->syncTelegramDescriptions($bot);
+                    $message .= '; [OK] descriptions';
+                } catch (Throwable $exception) {
+                    $message .= '; [FAIL] descriptions - ' . $exception->getMessage();
+                }
+            }
+            if ($bot !== null && !$bot->webhookSynced() && !$this->remover->telegramBotRemoved($bot)) {
+                try {
+                    $this->webhookUpdater->syncTelegramWebhook($bot);
+                    $message .= '; [OK] webhook';
+                } catch (Throwable $exception) {
+                    $message .= '; [FAIL] webhook - ' . $exception->getMessage();
+                }
+            }
+
+            $logger($message);
+        });
+
         return $result;
+    }
+
+    private function getUsernames(string $filename): array
+    {
+        $usernames = [];
+
+        $this->walk($filename, static function (array $data) use (&$usernames): void {
+            $usernames[] = $data['username'];
+        });
+
+        return $usernames;
+    }
+
+    private function walk(string $filename, callable $func): void
+    {
+        $mandatoryColumns = [
+            'skip',
+            'group',
+            'username',
+            'name',
+            'token',
+            'stage',
+            'country',
+            'locale',
+            'primary',
+            'admin_id',
+            'admin_only',
+        ];
+
+        $this->walker->walk($filename, function (array $data) use ($func): void {
+            if ($data['stage'] !== $this->stage) {
+                return;
+            }
+
+            if ($data['skip'] === '1') {
+                return;
+            }
+
+            $func($data);
+        }, mandatoryColumns: $mandatoryColumns);
     }
 }
--- src/Service/Telegram/Channel/TelegramChannelImporter.php
+++ src/Service/Telegram/Channel/TelegramChannelImporter.php
@@ -9,12 +9,10 @@ use App\Entity\Location;
 use App\Enum\Telegram\TelegramBotGroupName;
 use App\Repository\Telegram\Channel\TelegramChannelRepository;
 use App\Service\Address\AddressProvider;
+use App\Service\CsvFileWalker;
 use App\Service\Intl\CountryProvider;
 use App\Service\Intl\LocaleProvider;
 use App\Transfer\Telegram\TelegramChannelTransfer;
-use InvalidArgumentException;
-use LogicException;
-use RuntimeException;
 
 class TelegramChannelImporter
 {
@@ -26,6 +24,7 @@ class TelegramChannelImporter
         private readonly TelegramChannelRemover $remover,
         private readonly CountryProvider $countryProvider,
         private readonly LocaleProvider $localeProvider,
+        private readonly CsvFileWalker $walker,
         private readonly string $stage,
     )
     {
@@ -33,158 +32,135 @@ class TelegramChannelImporter
 
     public function importTelegramChannels(string $filename, callable $logger = null): ImportResult
     {
-        if (!file_exists($filename)) {
-            throw new InvalidArgumentException(sprintf('"%s" file is not exists', $filename));
-        }
-
-        $handle = fopen($filename, 'r');
-
-        if ($handle === false) {
-            throw new RuntimeException(sprintf('Unable to open "%s" file', $filename));
-        }
-
         $result = new ImportResult();
 
-        try {
-            $usernames = [];
-            $mandatoryColumns = [
-                'skip',
-                'group',
-                'username',
-                'name',
-                'stage',
-                'country',
-                'locale',
-                'primary',
-                'latitude',
-                'longitude',
-                'highest_address_component',
-            ];
+        $channels = $this->repository->findAll();
+        $usernames = $this->getUsernames($filename);
+        foreach ($channels as $channel) {
+            if (!in_array($channel->getUsername(), $usernames, true) && !$this->remover->telegramChannelRemoved($channel)) {
+                $this->remover->removeTelegramChannel($channel);
+                $message = $channel->getUsername();
+                $message .= ': [OK] ';
+                $message .= 'deleted';
+                $result->incDeletedCount();
+                $logger($message);
+            }
+        }
+
+        $logger = $logger ?? fn (string $message) => null;
+
+        $this->walk($filename, function ($data) use ($result, $logger): void {
             $addressComponents = [
                 'country',
                 'administrative_area_level_1',
                 'administrative_area_level_2',
                 'administrative_area_level_3',
             ];
+            $administrativeAreaLevel1 = null;
+            $administrativeAreaLevel2 = null;
+            $administrativeAreaLevel3 = null;
 
-            $logger = $logger ?? fn (string $message) => null;
-
-            $columns = fgetcsv($handle);
-            $count = count($columns);
-
-            $index = 1;
-
-            while (($row = fgetcsv($handle)) !== false) {
-                $index++;
-
-                if (!isset($row[0]) || [null] === $row) {
-                    continue;
+            if (!empty($data['latitude']) && !empty($data['longitude'])) {
+                if (empty($data['highest_address_component'])) {
+                    return;
                 }
 
-                $rowCount = count($row);
+                $location = new Location($data['latitude'], $data['longitude']);
+                $address = $this->addressProvider->getAddress($location);
 
-                if ($count !== $rowCount) {
-                    throw new LogicException(sprintf('Row #%d has wrong number of columns. Should have %d columns, got %d', $index, $count, $rowCount));
+                if ($address === null) {
+                    return;
                 }
 
-                $data = array_combine($columns, $row);
+                $highestAddressComponent = $data['highest_address_component'];
 
-                foreach ($mandatoryColumns as $mandatoryColumn) {
-                    if (!array_key_exists($mandatoryColumn, $data)) {
-                        throw new LogicException(sprintf('Row #%d has not "%s" column', $index, $mandatoryColumn));
-                    }
-                }
-
-                if ($data['stage'] !== $this->stage) {
-                    continue;
-                }
-
-                if ($data['skip'] === '1') {
-                    continue;
-                }
-
-                $administrativeAreaLevel1 = null;
-                $administrativeAreaLevel2 = null;
-                $administrativeAreaLevel3 = null;
-
-                if (!empty($data['latitude']) && !empty($data['longitude'])) {
-                    if (empty($data['highest_address_component'])) {
-                        continue;
-                    }
-
-                    $location = new Location($data['latitude'], $data['longitude']);
-                    $address = $this->addressProvider->getAddress($location);
-
-                    if ($address === null) {
-                        continue;
-                    }
-
-                    $highestAddressComponent = $data['highest_address_component'];
-
-                    $addressComponentPosition = array_search($highestAddressComponent, $addressComponents);
-                    $administrativeAreaLevel1 = $addressComponentPosition >= array_search('administrative_area_level_1', $addressComponents)
-                        ? $address?->getAdministrativeAreaLevel1()
-                        : null;
-                    $administrativeAreaLevel2 = $addressComponentPosition >= array_search('administrative_area_level_2', $addressComponents)
-                        ? $address?->getAdministrativeAreaLevel2()
-                        : null;
-                    $administrativeAreaLevel3 = $addressComponentPosition >= array_search('administrative_area_level_3', $addressComponents)
-                        ? $address?->getAdministrativeAreaLevel3()
-                        : null;
-                }
-
-                $transfer = (new TelegramChannelTransfer($data['username']))
-                    ->setGroup(TelegramBotGroupName::fromName($data['group']))
-                    ->setName($data['name'])
-                    ->setCountry($this->countryProvider->getCountry($data['country']))
-                    ->setLocale($this->localeProvider->getLocale($data['locale']))
-                    ->setAdministrativeAreaLevel1($administrativeAreaLevel1)
-                    ->setAdministrativeAreaLevel2($administrativeAreaLevel2)
-                    ->setAdministrativeAreaLevel3($administrativeAreaLevel3)
-                    ->setPrimary($data['primary'] === '1')
-                ;
-
-                $channel = $this->repository->findAnyOneByUsername($transfer->getUsername());
-
-                $message = $transfer->getUsername();
-                $message .= ': [OK] ';
-
-                if ($channel === null) {
-                    $this->creator->createTelegramChannel($transfer);
-                    $message .= 'created';
-                    $result->incCreatedCount();
-                } else {
-                    $this->updater->updateTelegramChannel($channel, $transfer);
-                    $message .= 'updated';
-                    $result->incUpdatedCount();
-
-                    if ($this->remover->telegramChannelRemoved($channel)) {
-                        $this->remover->undoTelegramChannelRemove($channel);
-                        $message .= '; [OK] restored';
-                        $result->incRestoredCount();
-                    }
-                }
-
-                $usernames[] = $channel->getUsername();
-
-                $logger($message);
+                $addressComponentPosition = array_search($highestAddressComponent, $addressComponents);
+                $administrativeAreaLevel1 = $addressComponentPosition >= array_search('administrative_area_level_1', $addressComponents)
+                    ? $address?->getAdministrativeAreaLevel1()
+                    : null;
+                $administrativeAreaLevel2 = $addressComponentPosition >= array_search('administrative_area_level_2', $addressComponents)
+                    ? $address?->getAdministrativeAreaLevel2()
+                    : null;
+                $administrativeAreaLevel3 = $addressComponentPosition >= array_search('administrative_area_level_3', $addressComponents)
+                    ? $address?->getAdministrativeAreaLevel3()
+                    : null;
             }
 
-            $channels = $this->repository->findAll();
-            foreach ($channels as $channel) {
-                if (!in_array($channel->getUsername(), $usernames, true) && !$this->remover->telegramChannelRemoved($channel)) {
-                    $this->remover->removeTelegramChannel($channel);
-                    $message = $channel->getUsername();
-                    $message .= ': [OK] ';
-                    $message .= 'deleted';
-                    $result->incDeletedCount();
-                    $logger($message);
+            $transfer = (new TelegramChannelTransfer($data['username']))
+                ->setGroup(TelegramBotGroupName::fromName($data['group']))
+                ->setName($data['name'])
+                ->setCountry($this->countryProvider->getCountry($data['country']))
+                ->setLocale($this->localeProvider->getLocale($data['locale']))
+                ->setAdministrativeAreaLevel1($administrativeAreaLevel1)
+                ->setAdministrativeAreaLevel2($administrativeAreaLevel2)
+                ->setAdministrativeAreaLevel3($administrativeAreaLevel3)
+                ->setPrimary($data['primary'] === '1')
+            ;
+
+            $channel = $this->repository->findAnyOneByUsername($transfer->getUsername());
+
+            $message = $transfer->getUsername();
+            $message .= ': [OK] ';
+
+            if ($channel === null) {
+                $this->creator->createTelegramChannel($transfer);
+                $message .= 'created';
+                $result->incCreatedCount();
+            } else {
+                $this->updater->updateTelegramChannel($channel, $transfer);
+                $message .= 'updated';
+                $result->incUpdatedCount();
+
+                if ($this->remover->telegramChannelRemoved($channel)) {
+                    $this->remover->undoTelegramChannelRemove($channel);
+                    $message .= '; [OK] restored';
+                    $result->incRestoredCount();
                 }
             }
-        } finally {
-            fclose($handle);
-        }
+
+            $logger($message);
+        });
 
         return $result;
+    }
+
+    private function getUsernames(string $filename): array
+    {
+        $usernames = [];
+
+        $this->walk($filename, static function (array $data) use (&$usernames): void {
+            $usernames[] = $data['username'];
+        });
+
+        return $usernames;
+    }
+
+    private function walk(string $filename, callable $func): void
+    {
+        $mandatoryColumns = [
+            'skip',
+            'group',
+            'username',
+            'name',
+            'stage',
+            'country',
+            'locale',
+            'primary',
+            'latitude',
+            'longitude',
+            'highest_address_component',
+        ];
+
+        $this->walker->walk($filename, function (array $data) use ($func): void {
+            if ($data['stage'] !== $this->stage) {
+                return;
+            }
+
+            if ($data['skip'] === '1') {
+                return;
+            }
+
+            $func($data);
+        }, mandatoryColumns: $mandatoryColumns);
     }
 }
