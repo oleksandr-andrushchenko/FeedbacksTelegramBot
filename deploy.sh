79c6337d27c996bc1998d7c23fdb740d4c73187f
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-10-12 09:11:44 -0600

    stage argument added & implemented

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,32 @@
-#!/bin/sh
-#set -e
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "stage argument is required"
+  exit 1
+fi
+
+array=("dev" "prod")
+value="\<${1}\>"
+
+# shellcheck disable=SC2199
+if [[ ${array[@]} =~ $value ]]; then
+  echo "continue with $1 stage"
+else
+  echo "invalid stage"
+  exit 1
+fi
+
 docker compose run php composer install --prefer-dist --optimize-autoloader --no-dev
 docker compose run php php bin/console cache:clear --env=prod
 docker compose run php php bin/console cache:warmup --env=prod
 
-#serverless deploy --stage=prod
-serverless deploy
-
-#get back dev env
-docker compose run php composer install
-docker compose run php php bin/console cache:warmup
+serverless deploy --stage=$1
 
 #serverless bref:cli --args="doctrine:database:create" --stage=prod
-#serverless bref:cli --args="doctrine:migrations:migrate --no-interaction"
+serverless bref:cli --args="doctrine:migrations:migrate --no-interaction" --stage=$1
 #serverless bref:cli --args="telegram:bot:show-all --no-interaction"
 
+docker compose run php composer install
+docker compose run php php bin/console cache:warmup
 
 #serverless logs -f web --tail
