014eb71bd589cc93506a71ad26dff5e93dd0f5d0
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-10-25 20:15:30 -0600

    parameters added to output

--- tests/Fake/Service/FakeTranslator.php
+++ tests/Fake/Service/FakeTranslator.php
@@ -15,7 +15,7 @@ class FakeTranslator implements TranslatorInterface
 
     public function trans(string $id, array $parameters = [], string $domain = null, string $locale = null): string
     {
-        return $id;
+        return $id . implode(' ', $parameters);
     }
 }
 
