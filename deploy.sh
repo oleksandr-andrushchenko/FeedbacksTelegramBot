8e32b9eaf97745c7dea2ed97daf374d4419bf307
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-10-28 16:29:09 -0600

    re-factoring; fix

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-array=("dev" "prod")
+envs=("dev" "prod")
 value="\<${1}\>"
 
 # shellcheck disable=SC2199
-if [[ ${array[@]} =~ $value ]]; then
+if [[ ${envs[@]} =~ $value ]]; then
   echo "continue with $1 stage"
 else
   echo "invalid stage"
@@ -17,14 +17,13 @@ else
 fi
 
 docker compose run php composer install --prefer-dist --optimize-autoloader --no-dev
-docker compose run php APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear --env=prod
-docker compose run php APP_ENV=prod APP_DEBUG=0 php bin/console cache:warmup --env=prod
+docker compose run php php bin/console cache:clear --env=prod
+docker compose run php php bin/console cache:warmup --env=prod
 
 serverless deploy --stage=$1
 
 #serverless bref:cli --args="doctrine:database:create" --stage=prod
 serverless bref:cli --args="doctrine:migrations:migrate --no-interaction --all-or-nothing" --stage=$1
-#serverless bref:cli --args="telegram:bot:show-all --no-interaction"
 
 docker compose run php composer install
 docker compose run php php bin/console cache:warmup
