0253f9f511202485d5b16fe22272f4df0f2203bd
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-10-28 21:31:49 -0600

    position increased up to 2; short input cases bugs fixed

--- src/Service/Util/String/SecretsAdder.php
+++ src/Service/Util/String/SecretsAdder.php
@@ -6,8 +6,18 @@ namespace App\Service\Util\String;
 
 class SecretsAdder
 {
-    function addSecrets(string $input, int $position = 1, string $char = '*', int $count = 3): string
+    function addSecrets(string $input, int $position = 2, string $char = '*', int $count = 3): string
     {
+        $length = mb_strlen($input);
+
+        if ($length <= $count) {
+            return str_repeat($char, $length);
+        }
+
+        if ($length - $position < $count) {
+            $position = $length - $count;
+        }
+
         return mb_substr($input, 0, $position) . str_repeat($char, $count) . mb_substr($input, $position + $count);
     }
 }
