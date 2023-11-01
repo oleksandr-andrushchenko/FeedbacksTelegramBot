274060eef0aef4a1db50f209eb6f84fa50091e77
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-11-01 12:33:09 -0600

    allow comma in search term

--- src/Validator/Feedback/SearchTermTransferConstraint.php
+++ src/Validator/Feedback/SearchTermTransferConstraint.php
@@ -12,7 +12,7 @@ class SearchTermTransferConstraint extends Constraint
 
     public string $textSingleLineMessage = 'text.single_line';
 
-    public string $textAllowedChars = ',;*^$!~';
+    public string $textAllowedChars = ';*^$!~';
     public string $textAllowedCharsMessage = 'text.allowed_chars';
 
     public string $textEmojiRegex = '/([*#0-9](?>\\xEF\\xB8\\x8F)?\\xE2\\x83\\xA3|\\xC2[\\xA9\\xAE]|\\xE2..(\\xF0\\x9F\\x8F[\\xBB-\\xBF])?(?>\\xEF\\xB8\\x8F)?|\\xE3(?>\\x80[\\xB0\\xBD]|\\x8A[\\x97\\x99])(?>\\xEF\\xB8\\x8F)?|\\xF0\\x9F(?>[\\x80-\\x86].(?>\\xEF\\xB8\\x8F)?|\\x87.\\xF0\\x9F\\x87.|..(\\xF0\\x9F\\x8F[\\xBB-\\xBF])?|(((?<zwj>\\xE2\\x80\\x8D)\\xE2\\x9D\\xA4\\xEF\\xB8\\x8F\k<zwj>\\xF0\\x9F..(\k<zwj>\\xF0\\x9F\\x91.)?|(\\xE2\\x80\\x8D\\xF0\\x9F\\x91.){2,3}))?))/';
