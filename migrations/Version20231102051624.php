b4bea6dc45b5e3056305ef7a261d71548eceed41
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-11-01 23:17:27 -0600

    mesenger user index added

---
+++ migrations/Version20231102051624.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231102051624 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE INDEX IDX_2F5DF3DDE22A4301772E836A ON messenger_users (messenger, identifier)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX IDX_2F5DF3DDE22A4301772E836A ON messenger_users');
+    }
+}
