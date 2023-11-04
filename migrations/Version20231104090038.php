970971eb585ffe0dd27b2f6101ba760ded0d644b
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-11-04 03:02:25 -0600

    created at index added

---
+++ migrations/Version20231104090038.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231104090038 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE INDEX IDX_7E6C3F898B8E8428 ON feedbacks (created_at)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX IDX_7E6C3F898B8E8428 ON feedbacks');
+    }
+}
