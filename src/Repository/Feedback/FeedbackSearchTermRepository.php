966a0cd133c7659df2cb759126fc49c9bbd4883f
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-11-08 00:48:03 -0700

    new find by period method added

--- src/Repository/Feedback/FeedbackSearchTermRepository.php
+++ src/Repository/Feedback/FeedbackSearchTermRepository.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\Repository\Feedback;
 
 use App\Entity\Feedback\FeedbackSearchTerm;
+use DateTimeInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -28,5 +31,26 @@ class FeedbackSearchTermRepository extends ServiceEntityRepository
         return $this->findBy([
             'normalizedText' => $normalizedText,
         ]);
+    }
+
+    /**
+     * @param DateTimeInterface $from
+     * @param DateTimeInterface $to
+     * @return Paginator|FeedbackSearchTerm[]
+     */
+    public function findByPeriod(DateTimeInterface $from, DateTimeInterface $to): Paginator
+    {
+        $queryBuilder = $this->createQueryBuilder('fst');
+
+        $query = $queryBuilder
+            ->andWhere(
+                $queryBuilder->expr()->gte('fst.createdAt', ':createdAtFrom'),
+                $queryBuilder->expr()->lt('fst.createdAt', ':createdAtTo'),
+            )
+            ->setParameter('createdAtFrom', $from, Types::DATE_IMMUTABLE)
+            ->setParameter('createdAtTo', $to, Types::DATE_IMMUTABLE)
+        ;
+
+        return new Paginator($query);
     }
 }
