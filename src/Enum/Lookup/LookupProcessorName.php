d9469e116d4ba9a823b5cf9a996eebcf1069599f
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-11-15 05:47:20 -0700

    from name method removed

--- src/Enum/Lookup/LookupProcessorName.php
+++ src/Enum/Lookup/LookupProcessorName.php
@@ -8,15 +8,4 @@ enum LookupProcessorName: string
 {
     case feedbacks = 'feedbacks';
     case clarity = 'clarity';
-
-    public static function fromName(string $name): ?self
-    {
-        foreach (self::cases() as $enum) {
-            if ($enum->name === $name) {
-                return $enum;
-            }
-        }
-
-        return null;
-    }
 }
