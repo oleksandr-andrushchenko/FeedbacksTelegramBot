687b56af3e5c0058edd489ae4cbce89fd21ac5f4
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-11-15 06:32:00 -0700

    normalized text cases bug fixed

--- src/Service/Feedback/FeedbackSearcher.php
+++ src/Service/Feedback/FeedbackSearcher.php
@@ -37,7 +37,7 @@ class FeedbackSearcher
 
         $feedbacks = array_filter($feedbacks, static function (Feedback $feedback) use ($feedbackSearchTerm): bool {
             foreach ($feedback->getSearchTerms() as $searchTerm) {
-                if ($searchTerm->getNormalizedText() === $feedbackSearchTerm->getNormalizedText()) {
+                if (strcasecmp($searchTerm->getNormalizedText(), $feedbackSearchTerm->getNormalizedText()) === 0) {
                     if (
                         $feedbackSearchTerm->getType() !== SearchTermType::unknown
                         && $searchTerm->getType() !== SearchTermType::unknown
