5271c9a583ed6fa8240ba433fcecf65991f332bf
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-11-15 15:46:57 -0700

    records got hidden for non-premium users

--- src/Service/Lookup/Viewer/LookupViewer.php
+++ src/Service/Lookup/Viewer/LookupViewer.php
@@ -24,7 +24,7 @@ abstract class LookupViewer
     {
         // 🔴🟡🟢⚪️🚨‼️
         // ⬜️⬛️◻️◼️◽️◾️▫️▪️
-        return 'ℹ️ ' . implode("\n▫️ ", $this->normalizeAndFilterEmptyStrings($list));
+        return '◻️ ' . implode("\n▫️ ", $this->normalizeAndFilterEmptyStrings($list));
     }
 
     protected function wrapResultRecord(?string $title, array $items, callable $record, bool $full): string
@@ -35,18 +35,26 @@ abstract class LookupViewer
             $message[] = $this->wrapTitle($title);
         }
 
-//        if ($full) {
-            $maxResults = count($items);
-//        } else {
-//            $maxResults = min(1);
-//        }
+        $count = count($items);
+
+        if ($full) {
+            $maxResults = $count;
+        } else {
+            $maxResults = intval($count * .1);
+            $maxResults = max($maxResults, 1);
+        }
 
         foreach (array_slice($items, 0, $maxResults) as $item) {
             $message[] = $this->wrapList($record($item));
         }
 
-        if ($maxResults !== count($items)) {
-            $message[] = '';
+        if ($maxResults !== $count) {
+            $parameters = [
+                'shown_count' => $maxResults,
+                'total_count' => $count,
+                'subscribe_command' => '/subscribe',
+            ];
+            $message[] = sprintf('<i>[ %s ]</i>', $this->translator->trans('hidden_list', $parameters, 'lookups.tg'));
         }
 
         return implode("\n\n", $message);
