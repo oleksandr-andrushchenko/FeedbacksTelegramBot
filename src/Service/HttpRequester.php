bb3caedbf3325ccff4c251bf6af6326f9a217056
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-11-21 07:10:31 -0700

    user agent as google bot

--- src/Service/HttpRequester.php
+++ src/Service/HttpRequester.php
@@ -34,6 +34,12 @@ class HttpRequester
             'timeout' => $timeout,
         ]);
 
+        if (!isset($options['headers'])) {
+            $options['headers'] = [];
+        }
+
+        $options['headers']['User-Agent'] = 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
+
         $response = $this->httpClient->request($method, $url, $options);
 
         $status = $response->getStatusCode();
