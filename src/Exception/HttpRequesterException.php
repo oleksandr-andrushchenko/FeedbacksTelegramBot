e56368901b610ded9be41e5884fced9b3cf990e1
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-12-01 13:22:10 -0700

    removed redundatn public info

--- src/Exception/HttpRequesterException.php
+++ src/Exception/HttpRequesterException.php
@@ -9,23 +9,13 @@ use Throwable;
 class HttpRequesterException extends Exception
 {
     public function __construct(
-        private readonly string $method,
-        private readonly string $url,
+        string $method,
+        string $url,
         private readonly int $statusCode,
         ?Throwable $previous = null
     )
     {
-        parent::__construct(sprintf('%d status code received for "%s %s"', $this->statusCode, $this->method, $url), 0, $previous);
-    }
-
-    public function getMethod(): string
-    {
-        return $this->method;
-    }
-
-    public function getUrl(): string
-    {
-        return $this->url;
+        parent::__construct(sprintf('%d status code received for "%s %s"', $this->statusCode, $method, $url), 0, $previous);
     }
 
     public function getStatusCode(): int
