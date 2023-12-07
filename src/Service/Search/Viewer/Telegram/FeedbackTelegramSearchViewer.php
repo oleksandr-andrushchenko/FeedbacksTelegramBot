447ba9207d3e5b51467ea2f27a8fd9578a36989f
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-12-06 21:22:00 -0700

    independed feedback message builder implemented

--- src/Service/Search/Viewer/Telegram/FeedbackTelegramSearchViewer.php
+++ src/Service/Search/Viewer/Telegram/FeedbackTelegramSearchViewer.php
@@ -7,6 +7,7 @@ namespace App\Service\Search\Viewer\Telegram;
 use App\Entity\Feedback\Feedback;
 use App\Entity\Feedback\FeedbackSearchTerm;
 use App\Service\Feedback\Telegram\Bot\View\FeedbackTelegramViewProvider;
+use App\Service\Intl\TimeProvider;
 use App\Service\Search\Viewer\SearchViewer;
 use App\Service\Search\Viewer\SearchViewerCompose;
 use App\Service\Search\Viewer\SearchViewerInterface;
@@ -25,20 +26,46 @@ class FeedbackTelegramSearchViewer extends SearchViewer implements SearchViewerI
 
     public function getResultMessage($record, FeedbackSearchTerm $searchTerm, array $context = []): string
     {
-        $full = $context['full'] ?? false;
-
         $message = '💫 ';
+
+        $full = $context['full'] ?? false;
+        $locale = $context['locale'] ?? null;
+
+        $m = $this->modifier;
+
         $message .= $this->implodeResult(
             $this->trans('feedbacks_title'),
             $record,
-            fn (Feedback $feedback): array => [
-                $this->feedbackTelegramViewProvider->getFeedbackTelegramView(
-                    $context['bot'] ?? $feedback->getTelegramBot(),
-                    $feedback,
-                    addSecrets: !$full,
-                    addTime: true,
-                    addCountry: true,
-                ),
+            fn (Feedback $item): array => [
+                $m->create()
+//                    ->add($m->slashesModifier())
+//                    ->add($m->boldModifier())
+                    ->apply(
+                        $this->feedbackTelegramViewProvider->getFeedbackSearchTermsTelegramView(
+                            $item->getSearchTerms()->toArray(),
+                            addSecrets: !$full,
+                            localeCode: $locale
+                        )
+                    ),
+                $m->create()
+                    ->add($m->markModifier())
+                    ->add($m->appendModifier($this->trans('mark_' . ($item->getRating()->value > 0 ? '+1' : $item->getRating()->value))))
+                    ->add($m->bracketsModifier($this->trans('mark')))
+                    ->apply($item->getRating()->value),
+                $m->create()
+                    ->add($m->slashesModifier())
+                    ->add($m->spoilerModifier())
+                    ->add($m->bracketsModifier($this->trans('description')))
+                    ->apply($item->getDescription()),
+                $m->create()
+                    ->add($m->slashesModifier())
+                    ->add($m->countryModifier(locale: $locale))
+                    ->add($m->bracketsModifier($this->trans('country')))
+                    ->apply($item->getCountryCode()),
+                $m->create()
+                    ->add($m->datetimeModifier(TimeProvider::DATE, timezone: $item->getUser()->getTimezone(), locale: $locale))
+                    ->add($m->bracketsModifier($this->trans('created_at')))
+                    ->apply($item->getCreatedAt()),
             ],
             $full
         );
