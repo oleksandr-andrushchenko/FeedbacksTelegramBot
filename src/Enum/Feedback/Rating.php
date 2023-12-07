11567966b9c5050a88e43bc3452eb54223058a28
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-12-06 23:08:07 -0700

    feedback ratings synced with current logic

--- src/Enum/Feedback/Rating.php
+++ src/Enum/Feedback/Rating.php
@@ -6,13 +6,9 @@ namespace App\Enum\Feedback;
 
 enum Rating: int
 {
-//    case extremely_unsatisfied = -3;
-//    case very_unsatisfied = -2;
     case unsatisfied = -1;
     case neutral = 0;
     case satisfied = 1;
-//    case very_satisfied = 2;
-//    case extremely_satisfied = 3;
 
     public static function random(): self
     {
