c8780ce9012233d1496d468b93788fdcbbc86ab4
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-12-07 22:22:30 -0700

    new add word secrets method added

--- src/Service/Util/String/SecretsAdder.php
+++ src/Service/Util/String/SecretsAdder.php
@@ -20,4 +20,23 @@ class SecretsAdder
 
         return mb_substr($input, 0, $position) . str_repeat($char, $count) . mb_substr($input, $position + $count);
     }
+
+    public function addWordSecrets(string $text, string|array $excepts = null, string $char = '*'): string
+    {
+        $excepts = $excepts === null ? [] : (is_string($excepts) ? [$excepts] : $excepts);
+
+        $keep = [];
+
+        foreach ($excepts as $except) {
+            $keep = array_merge($keep, array_filter(array_map('trim', explode(' ', $except))));
+        }
+
+        $all = array_filter(array_map('trim', explode(' ', $text)));
+        $search = array_diff($all, $keep);
+
+        $replace = array_map(static fn (string $occurrence): string => str_repeat($char, mb_strlen($occurrence)), $search);
+
+
+        return str_ireplace($search, $replace, $text);
+    }
 }
