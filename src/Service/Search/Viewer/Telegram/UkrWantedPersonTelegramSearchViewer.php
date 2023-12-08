c4e887b324f4415947ad9024da5bd0997afd2642
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-12-08 05:12:45 -0700

    secrets improvements, display all data, labels added

--- src/Service/Search/Viewer/Telegram/UkrWantedPersonTelegramSearchViewer.php
+++ src/Service/Search/Viewer/Telegram/UkrWantedPersonTelegramSearchViewer.php
@@ -7,6 +7,7 @@ namespace App\Service\Search\Viewer\Telegram;
 use App\Entity\Feedback\FeedbackSearchTerm;
 use App\Entity\Search\UkrWantedPerson\UkrWantedPerson;
 use App\Entity\Search\UkrWantedPerson\UkrWantedPersons;
+use App\Service\Intl\TimeProvider;
 use App\Service\Search\Viewer\SearchViewer;
 use App\Service\Search\Viewer\SearchViewerCompose;
 use App\Service\Search\Viewer\SearchViewerInterface;
@@ -28,101 +29,119 @@ class UkrWantedPersonTelegramSearchViewer extends SearchViewer implements Search
         $full = $context['full'] ?? false;
 
         return match (get_class($record)) {
-            UkrWantedPersons::class => $this->getPersonsMessage($record, $full),
-            UkrWantedPerson::class => $this->getPersonMessage($record, $full),
+            UkrWantedPersons::class => $this->getPersonsMessage($record, $searchTerm, $full),
+            UkrWantedPerson::class => $this->getPersonMessage($record, $searchTerm, $full),
         };
     }
 
-    public function getPersonWrapMessageCallback(bool $full): callable
+    public function getPersonWrapMessageCallback(FeedbackSearchTerm $searchTerm, bool $full): callable
     {
         $m = $this->modifier;
 
+        $term = $searchTerm->getNormalizedText();
+
         return fn (UkrWantedPerson $item): array => [
             $m->create()
-                ->add($m->appendModifier(' '))
-                ->add($m->appendModifier($item->getUkrName()))
-                ->add($m->appendModifier(' '))
-                ->add($m->appendModifier($item->getUkrPatronymic()))
+                ->add($m->filterModifier())
+                ->add($m->implodeModifier(' '))
+                ->add($m->trimModifier())
+                ->add($m->emptyNullModifier())
                 ->add(
                     $m->bracketsModifier(
                         $m->create()
-                            ->add($m->appendModifier(' '))
-                            ->add($m->appendModifier($item->getRusSurname()))
-                            ->add($m->appendModifier(' '))
-                            ->add($m->appendModifier($item->getRusName()))
-                            ->add($m->appendModifier(' '))
-                            ->add($m->appendModifier($item->getRusPatronymic()))
-                            ->apply($this->trans('rus_name') . ':')
+                            ->add($m->filterModifier())
+                            ->add($m->implodeModifier(' '))
+                            ->add($m->trimModifier())
+                            ->add($m->emptyNullModifier())
+                            ->add($m->prependModifier(': '))
+                            ->add($m->prependModifier($this->trans('rus_name')))
+                            ->apply([$item->getRusSurname(), $item->getRusName(), $item->getRusPatronymic()])
                     )
                 )
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier(excepts: $term))
                 ->add($m->slashesModifier())
-                ->add($full ? $m->nullModifier() : $m->secretsModifier())
                 ->add($full ? $m->linkModifier($item->getHref()) : $m->nullModifier())
                 ->add($m->boldModifier())
-                ->apply($item->getUkrSurname()),
+                ->add($m->bracketsModifier($this->trans('name')))
+                ->apply([$item->getUkrSurname(), $item->getUkrName(), $item->getUkrPatronymic()]),
             $m->create()
+                ->add($m->emptyNullModifier())
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier())
                 ->add($m->slashesModifier())
                 ->add($m->bracketsModifier($this->trans('gender')))
                 ->apply($item->getGender()),
             $m->create()
-                ->add($m->datetimeModifier('d.m.Y'))
+                ->add($m->datetimeModifier(TimeProvider::DATE))
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier())
+                ->add($m->slashesModifier())
                 ->add($m->bracketsModifier($this->trans('born_at')))
                 ->apply($item->getBornAt()),
             $m->create()
-                ->add($m->redModifier())
-                ->add($m->appendModifier(' '))
-                ->add(
-                    $m->appendModifier(
-                        $m->create()
-                            ->add($m->slashesModifier())
-                            ->apply($item->getCategory())
-                    )
-                )
-                ->add($m->bracketsModifier($item->getCodexArticle()))
-                ->apply(true),
+                ->add($m->emptyNullModifier())
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier())
+                ->add($m->prependModifier(' '))
+                ->add($m->prependModifier($m->redModifier()(true)))
+                ->add($m->slashesModifier())
+                ->add($m->bracketsModifier($this->trans('category')))
+                ->apply($item->getCategory()),
             $m->create()
-                ->add($m->datetimeModifier('d.m.Y'))
+                ->add($m->emptyNullModifier())
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier())
+                ->add($m->slashesModifier())
+                ->add($m->bracketsModifier($this->trans('codex_article')))
+                ->apply($item->getCodexArticle()),
+            $m->create()
+                ->add($m->datetimeModifier(TimeProvider::DATE))
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier())
+                ->add($m->slashesModifier())
                 ->add($m->bracketsModifier($this->trans('absent_at')))
                 ->apply($item->getAbsentAt()),
             $m->create()
+                ->add($m->emptyNullModifier())
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier())
                 ->add($m->slashesModifier())
                 ->add($m->bracketsModifier($this->trans('absent_place')))
                 ->apply($item->getAbsentPlace()),
             $m->create()
+                ->add($m->emptyNullModifier())
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier())
                 ->add($m->slashesModifier())
                 ->add($m->bracketsModifier($this->trans('precaution')))
                 ->apply($item->getPrecaution()),
             $m->create()
+                ->add($m->emptyNullModifier())
+                ->add($full ? $m->nullModifier() : $m->wordSecretsModifier())
                 ->add($m->slashesModifier())
                 ->add($m->bracketsModifier($this->trans('region')))
                 ->apply($item->getRegion()),
             $m->create()
+                ->add($m->emptyNullModifier())
                 ->add($m->slashesModifier())
                 ->add($m->bracketsModifier($this->trans('call_to')))
                 ->apply($item->getCallTo()),
         ];
     }
 
-    private function getPersonsMessage(UkrWantedPersons $record, bool $full): string
+    private function getPersonsMessage(UkrWantedPersons $record, FeedbackSearchTerm $searchTerm, bool $full): string
     {
         $message = '🚨 ';
         $message .= $this->implodeResult(
             $this->trans('persons_title'),
             $record->getItems(),
-            $this->getPersonWrapMessageCallback($full),
+            $this->getPersonWrapMessageCallback($searchTerm, $full),
             $full
         );
 
         return $message;
     }
 
-    private function getPersonMessage(UkrWantedPerson $record, bool $full): string
+    private function getPersonMessage(UkrWantedPerson $record, FeedbackSearchTerm $searchTerm, bool $full): string
     {
         $message = '💫 ';
         $message .= $this->implodeResult(
             $this->trans('person_title'),
             [$record],
-            $this->getPersonWrapMessageCallback($full),
+            $this->getPersonWrapMessageCallback($searchTerm, $full),
             $full
         );
 
