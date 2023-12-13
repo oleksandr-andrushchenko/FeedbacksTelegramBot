bec351649c10a03fc0e949850d46771847a2a854
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2023-12-13 10:47:45 -0700

    base feedback user subscription class added & implemented

--- src/Message/Event/Feedback/FeedbackUserSubscriptionCreatedEvent.php
+++ src/Message/Event/Feedback/FeedbackUserSubscriptionCreatedEvent.php
@@ -4,43 +4,6 @@ declare(strict_types=1);
 
 namespace App\Message\Event\Feedback;
 
-use App\Entity\Feedback\FeedbackUserSubscription;
-use LogicException;
-
-readonly class FeedbackUserSubscriptionCreatedEvent
+readonly class FeedbackUserSubscriptionCreatedEvent extends FeedbackUserSubscriptionEvent
 {
-    private ?string $subscriptionId;
-
-    public function __construct(
-        ?string $subscriptionId = null,
-        private ?FeedbackUserSubscription $subscription = null,
-    )
-    {
-        if ($subscriptionId === null) {
-            if ($this->subscription === null) {
-                throw new LogicException('Either subscription id or subscription should be passed`');
-            }
-
-            $this->subscriptionId = $this->subscription->getId();
-        } else {
-            $this->subscriptionId = $subscriptionId;
-        }
-    }
-
-    public function getSubscription(): ?FeedbackUserSubscription
-    {
-        return $this->subscription;
-    }
-
-    public function getSubscriptionId(): ?string
-    {
-        return $this->subscriptionId;
-    }
-
-    public function __sleep(): array
-    {
-        return [
-            'subscriptionId',
-        ];
-    }
 }
